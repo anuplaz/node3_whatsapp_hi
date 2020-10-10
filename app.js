0dfa3bc6c1fcc1d2407dddc934ba423c51a17a66
Anoop E <anoop.e@tcs.com>
2020-10-10 19:11:21 +0800

    v2

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
 app.set('view engine', 'hbs')
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res)=>{
     res.render('home')
